4581a90636102a5f186ec83cf5045e8d1157cd14
wycats <wycats+admin@gmail.com>
2024-09-21 09:30:27 -0700

    Initial commit

---
+++ app/config/environment.d.ts
@@ -0,0 +1,14 @@
+/**
+ * Type declarations for
+ *    import config from 'vitejs-vite-k1nhrv/config/environment'
+ */
+declare const config: {
+  environment: string;
+  modulePrefix: string;
+  podModulePrefix: string;
+  locationType: 'history' | 'hash' | 'none';
+  rootURL: string;
+  APP: Record<string, unknown>;
+};
+
+export default config;
---
+++ tests/test-helper.js
@@ -0,0 +1,14 @@
+import Application from 'vitejs-vite-k1nhrv/app';
+import config from 'vitejs-vite-k1nhrv/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start as qunitStart } from 'ember-qunit';
+
+export function start() {
+  setApplication(Application.create(config.APP));
+
+  setup(QUnit.assert);
+
+  qunitStart();
+}
